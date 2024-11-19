06218023d6938f939d7a6e87d54729c8dc1c07a1
Alexandrov Alexandr <144331961+butareyka@users.noreply.github.com>
2024-11-19 14:53:41 +0300

    hide load.sh and README.md

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-sshpass -p IYoL\)4551 ssh -L 25100:localhost:25100 s408116@helios.cs.ifmo.ru -p 2222
-sshpass -p IYoL\)4551 ssh -L 36002:localhost:36002 s408116@helios.cs.ifmo.ru -p 2222
-
-CREATE TABLE point_model {
-id SERIAL PRIMARY KEY,
-x DOUBLE PRECISION NOT NULL,
-y DOUBLE PRECISION NOT NULL,
-r DOUBLE PRECISION NOT NULL
-}
+[//]: # (# your user instead of ***)
+sshpass -p *** ssh -L 25100:localhost:25100 s408116@helios.cs.ifmo.ru -p 2222
+sshpass -p *** ssh -L 36002:localhost:36002 s408116@helios.cs.ifmo.ru -p 2222
--- load.sh
+++ load.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -x #echo on
 
-WORKDIR=/home/studs/s408116/wildfly
+# your user instead of ***
+WORKDIR=/home/studs/***/wildfly
 WILDFLY34=/Users/macbook/ITMO/WEB/servers/wildfly-34.0.0.Final
 WILDFLY26=/Users/macbook/ITMO/WEB/servers/wildfly-preview-26.1.3.Final
 
 REMOTE_HOST=helios.cs.ifmo.ru
 REMOTE_PORT=2222
 
-REMOTE_USER=s408116
-REMOTE_PASSWORD=IYoL\)4551
+# your user and password instead of ***
+REMOTE_USER=***
+REMOTE_PASSWORD=***
 
 sshpass -p "$REMOTE_PASSWORD" scp -r -P 2222 $WILDFLY26 $REMOTE_USER@$REMOTE_HOST:$WORKDIR
